61c9b72f979e07294eaace464607ee1c39f21ebf
Jack Lian <jacklian@freelancer.com>
2016-04-02 01:06:03 +1100

    First commit
    -Installed Tween Engine

---
+++ desktop/src/com/jacklian/flappy/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jacklian.flappy.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jacklian.flappy.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
---
+++ html/src/com/jacklian/flappy/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.jacklian.flappy.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.jacklian.flappy.MyGdxGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new MyGdxGame();
+        }
+}
